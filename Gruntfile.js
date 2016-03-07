bbc36867c02a79954ffeeabc3d2f1765f2d87e3b
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-03-07 22:33:04 +0100

    register the task

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,22 +33,12 @@ module.exports = function (grunt) {
 
     // Configuration to be run (and then tested).
     foodfact: {
-      default_options: {
         options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+          'urls': [
+            'http://world.openfoodfacts.org/data/data-fields.txt',
+            'http://world.openfoodfacts.org/data/en.openfoodfacts.org.products.csv'
+          ]
         }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      }
     },
 
     // Unit tests.
