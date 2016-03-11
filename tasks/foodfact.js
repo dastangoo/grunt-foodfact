b267846ea3a4e5a550e4affa1f23df0615e4c6f3
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-03-11 21:50:26 +0100

    update tasl

--- tasks/foodfact.js
+++ tasks/foodfact.js
@@ -6,15 +6,31 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('foodfact', "Load the foodfact database", function() {
 
+    var done = this.async();
+
+
     this.files.forEach(function(file) {
 
       var destDir = path.dirname(file.dest);
+      var source  = file.src[0];
+
       grunt.log.debug('Destination : ' + file.dest);
       grunt.log.debug('Sources : ', file.src);
 
-      if (!grunt.file.exits(destDir)) {
+      if (!grunt.file.exists(destDir)) {
         grunt.file.mkdir(destDir);
       }
+
+      parse(file.src[0], {}, function(err, data){
+        if(err){
+          throw err;
+        }
+
+        grunt.log.debug(data);
+        grunt.file.write(file.dest, data);
+
+        done();
+      });
 
     });
   });
