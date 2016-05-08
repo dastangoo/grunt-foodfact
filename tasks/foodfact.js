98b6376196442ff2577a215f9456981637230f06
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-05-08 16:05:26 +0200

    reformat

--- tasks/foodfact.js
+++ tasks/foodfact.js
@@ -1,26 +1,9 @@
 module.exports = function (grunt) {
 
-  grunt.registerMultiTask('foodfact', "Load the foodfact database", function(){
-
-    var options = this.options({
-      download : false
-    });
-
-    var urls = this.data.urls || [];
-
-    grunt.log.debug('Task   : ' + this.name);
-    grunt.log.debug('Target : ' + this.target);
-    grunt.log.debug('URLs   : ', urls);
-
-    if(options.download){
-      grunt.log.debug('Downloading database');
-    }
-
-    this.files.forEach(function(file){
-
-      grunt.log.debug('Destination : ' + file.dest);
-      grunt.log.debug('Sources : ', file.src);
+  grunt.registerMultiTask(
+    'foodfact',
+    'Load the foodfact database',
+    function(){
 
     });
-  });
 };
