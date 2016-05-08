c99c9b613eaf5f8d92dafa0e72aa83299d98e28c
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-05-08 15:15:06 +0200

    clean up

--- tasks/foodfact.js
+++ tasks/foodfact.js
@@ -1,14 +1,8 @@
 module.exports = function (grunt) {
 
-  grunt.registerMultiTask('foodfact', "Load the foodfact database", function(p1, p2){
+  grunt.registerMultiTask('foodfact', "Load the foodfact database", function(){
 
-    var options = this.options({
-      urls : []
-    });
     grunt.log.writeln('Loading the database...');
-
-    grunt.log.debug('urls', options.urls);
-
     grunt.log.ok('Database loaded');
 
   });
