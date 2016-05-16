48bce81289151897e5a85047e0d1c0afe4932384
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-05-16 11:15:38 +0200

    clean up task

--- tasks/foodfact.js
+++ tasks/foodfact.js
@@ -7,12 +7,15 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('foodfact', "Load the foodfact database", function() {
 
     var done = this.async();
+    var count = 0;
+    var srcLength = this.filesSrc.length;
 
+    grunt.log.debug(srcLength + ' files to parse');
 
     this.files.forEach(function(file) {
 
       var destDir = path.dirname(file.dest);
-      var source  = file.src[0];
+      var source = file.src[0];
 
       grunt.log.debug('Destination : ' + file.dest);
       grunt.log.debug('Sources : ', file.src);
@@ -21,15 +24,17 @@ module.exports = function(grunt) {
         grunt.file.mkdir(destDir);
       }
 
-      parse(file.src[0], {}, function(err, data){
-        if(err){
+      parse(file.src[0], {}, function(err, data) {
+        var result;
+        if (err) {
           throw err;
         }
+        result = JSON.stringify(data);
 
-        grunt.log.debug(data);
-        grunt.file.write(file.dest, data);
-
-        done();
+        grunt.file.write(file.dest, result);
+        if (++count === srcLength) {
+          done();
+        }
       });
 
     });
