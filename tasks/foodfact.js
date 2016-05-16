d85372a82e34933178e04622701b655536dfff41
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2016-05-16 11:17:37 +0200

    clean up

--- tasks/foodfact.js
+++ tasks/foodfact.js
@@ -15,27 +15,9 @@ module.exports = function(grunt) {
     this.files.forEach(function(file) {
 
       var destDir = path.dirname(file.dest);
-      var source = file.src[0];
 
       grunt.log.debug('Destination : ' + file.dest);
       grunt.log.debug('Sources : ', file.src);
-
-      if (!grunt.file.exists(destDir)) {
-        grunt.file.mkdir(destDir);
-      }
-
-      parse(file.src[0], {}, function(err, data) {
-        var result;
-        if (err) {
-          throw err;
-        }
-        result = JSON.stringify(data);
-
-        grunt.file.write(file.dest, result);
-        if (++count === srcLength) {
-          done();
-        }
-      });
 
     });
   });
